906bcab0572a27e9f6b2553faee1364edac0992a
Get-My-Money <69455094+Get-My-Money@users.noreply.github.com>
2023-05-11 21:00:15 +0300

    Add files via upload

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Run the db.sql file using the psql command
+psql -U postgres -d project -f Data/db.sql
+
+# Run the sqoop command to import all tables
+sqoop import-all-tables \
+  -Dorg.apache.sqoop.splitter.allow_text_splitter=true \
+  -Dmapreduce.job.user.classpath.first=true \
+  --connect jdbc:postgresql://localhost/project \
+  --username postgres \
+  --warehouse-dir /project \
+  --as-avrodatafile \
+  --compression-codec=snappy \
+  --outdir /project/avsc \
+  --m 1
